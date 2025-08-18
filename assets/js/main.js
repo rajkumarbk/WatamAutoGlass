066d6bf701e088057b2fd572fc88df04a88a746d
rajkumarbk <rajkumarbk100@gmail.com>
2025-08-18 12:58:25 +0300

    tired

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,18 +42,6 @@
   });
 
   /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
-  });
-
-  /**
    * Preloader
    */
   const preloader = document.querySelector('#preloader');
